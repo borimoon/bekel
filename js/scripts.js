b1c326279276e67d537b3e59cc14c016bd3836a9
adriennficzere <boriana@Boriana.local>
2017-07-12 06:45:45 +0200

    mobile menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-
+// mobile menu show-hide
+$(function() {
+	var menuVisible = false;
+	$('#toggle').click(function() {
+		if (menuVisible) {
+			$('#menu').css({'display':'none'});
+			menuVisible = false;
+			return;
+		}
+		$('#menu').css({'display':'block'});
+		menuVisible = true;
+	});
+	$('#menu').click(function() {
+		$(this).css({'display':'none'});
+		menuVisible = false;
+	});
+});
